fa42ededaebd3e3f2a0dff687f603423c1dcb2d3
BREADfox27 <javierluna271107@gmail.com>
2026-09-15 11:30:23 +0200

    AudioManager

---
+++ My project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("AudioSource")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+
+    [Header("AudioClips")]
+    public AudioClip[] musicList;
+    public AudioClip[] sfxList;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(int musicIndex)
+    {
+        musicSource.clip = musicList[musicIndex];
+        musicSource.Play();
+    }
+
+    public void PlaySFX(int sfxIndex)
+    {
+        sfxSource.PlayOneShot(sfxList[sfxIndex]);
+    }
+}
