8b9ca157f26ad2af760741aec8edad26c73fa609
BREADfox27 <javierluna271107@gmail.com>
2026-09-18 00:43:52 +0200

    Shoot Mechanic Upgrade

---
+++ My project/Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    public int newBullets = 10;
+
+    [Header("Rotation")]
+    public float speedX;
+    public float speedY;
+    public float speedZ;
+
+    [Header("Levitation")]
+    public float amplitude;
+    public float speed;
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        RotateObject();
+        LevitateObj();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.gunAmmo += newBullets;
+        }
+    }
+
+    void RotateObject()
+    {
+        transform.Rotate(Vector3.right * speedX * Time.deltaTime);
+        transform.Rotate(Vector3.up * speedY * Time.deltaTime);
+        transform.Rotate(Vector3.forward * speedZ * Time.deltaTime);
+    }
+
+    void LevitateObj()
+    {
+        float newY = initialPosition.y + Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -5,13 +5,27 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    [Header("Game Data")]
     public int health = 10;
     public TextMeshProUGUI textHealth;
     public int gunAmmo = 50;
     public TextMeshProUGUI textAmmo;
 
+    [Header("Player")]
     public GameObject player;
     public GameObject respawnPoint;
+
+    [Header("Shoot")]
+    public GameObject gun;
+    public GameObject bullet;
+    public Transform bulletSpawn;
+
+    public float shotForce = 1500f;
+    public float shotRate = 0.25f;
+    public float recoilForce = 2f;
+    public float swayForce = 3f;
+
+    private float shotRateTime = 0;
 
     private void Awake()
     {
@@ -26,12 +40,18 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && gunAmmo > 0)
+        {
+            Shoot();
+        }
+
+        gun.transform.localPosition = Vector3.Lerp(gun.transform.localPosition, Vector3.zero, Time.deltaTime * 5f);
+        gun.transform.localRotation = Quaternion.Lerp(gun.transform.localRotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * 5f);
     }
 
     public void TakeDamage()
     {
-        health -= 1;
+        health--;
         textHealth.text = "Health: " + health.ToString();
 
         if (health <= 0)
@@ -46,7 +66,32 @@ public class GameManager : MonoBehaviour
         player.transform.position = respawnPoint.transform.position;
         player.transform.rotation = respawnPoint.transform.rotation;
         player.GetComponent<CharacterController>().enabled = true;
+
         health = 10;
         textHealth.text = "Health: " + health.ToString();
+    }
+
+    public void Shoot()
+    {
+        if (Time.time > shotRateTime)
+        {
+            GameObject newBullet = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+            newBullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * shotForce);
+            AddRecoil();
+            AudioManager.Instance.PlaySFX(0);
+
+            gunAmmo--;
+            textAmmo.text = "Ammo: " + gunAmmo.ToString();
+
+            shotRateTime = Time.time + shotRate;
+
+            Destroy(newBullet, 3);
+        }
+    }
+
+    private void AddRecoil()
+    {
+        gun.transform.Rotate(-recoilForce, 0f, 0f);
+        gun.transform.position -= gun.transform.forward * (recoilForce / 50f);
     }
 }
