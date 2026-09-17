c4ad86e28df317cd2e6c094eba51247004011f69
BREADfox27 <javierluna271107@gmail.com>
2026-09-17 13:30:18 +0200

    Clean Up

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -42,10 +42,10 @@ public class GameManager : MonoBehaviour
 
     public void Respawn()
     {
-        player.GetComponentInChildren<CharacterController>().enabled = false;
+        player.GetComponent<CharacterController>().enabled = false;
         player.transform.position = respawnPoint.transform.position;
         player.transform.rotation = respawnPoint.transform.rotation;
-        player.GetComponentInChildren<CharacterController>().enabled = true;
+        player.GetComponent<CharacterController>().enabled = true;
         health = 10;
         textHealth.text = "Health: " + health.ToString();
     }
