25bf2a6233d0bfa7e71ff1a95813a78c4c63a464
BREADfox27 <javierluna271107@gmail.com>
2026-09-19 13:14:58 +0200

    MainMenu Update

--- My project/Assets/Scripts/MainMenuBtnManager.cs
+++ My project/Assets/Scripts/MainMenuBtnManager.cs
@@ -3,9 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuBtnManager : MonoBehaviour
 {
+    public GameObject optionsPanel;
+
     public void Play()
     {
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void Options()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void Close()
+    {
+        optionsPanel.SetActive(false);
     }
 
     public void Exit()
